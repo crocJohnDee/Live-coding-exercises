0a5d5aec67e35deacde62c1c4d81e7c481450ab2
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-05-15 10:45:04 +0200

    first commit

---
+++ ex1.js
@@ -0,0 +1,20 @@
+// 1. Console log a line of text eg. "this is an exercise in console logging". (do not forget quotation marks). 
+console.log("this is an exercise in console logging");
+// 2. Declare a variable and give your variable the following value: "This is the value.". Console log your **variable**. 
+let string = "This is the value.";
+console.log(string);
+// 3. Declare a variable called firstName and assign your first name to that variable. Do they same with your last name. Console log both variables. NOTE: When console logging two variables, separate the variables with a comma (,) in the console log e.g. console.log(variable1, variable2);
+let firstName = "Henk";
+let lastName = "Johnsson";
+console.log(firstName, lastName);
+// 4. Make a profile of someone's information. Store all the information in variables eg. name, city, job etc. (five variables total) Console log the variables. 
+let name = "John Smith";
+let job = "Teacher";
+let city = "Berlin";
+let hobby = "Javascript";
+let age = 30;
+console.log(name, job, city, hobby, age);
+
+// 5. Print the sentence "John Smith is a 43 year old teacher who lives in Berlin" using variables (where possible) and text. **you can combine variables with text in the console by using the plus symbol (+) e.g. console.log("this is a " + variable);**
+console.log(name + " is a " + age + " year old " + job + " who lives in " + city);
+
---
+++ ex2.js
@@ -0,0 +1,48 @@
+
+// 1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let string = "123456";
+console.log(string);
+
+// 2. Access the 2nd and 6th elements of the string. Print the elements to the console. **Make sure you have selected the right index numbers** 
+console.log(string[1], string[5]);
+
+// 3. Declare a variable called name and assign a string as a value. 
+
+let name = "Piet";
+
+// 4. Declare a variable called age and assign a number as a value. 
+
+let age = 30;
+
+// 5. Declare a variable called year and assign a number as a value. 
+
+let year = 2040;
+
+// 6. With your declared variables, print the following: "/name/ is /age/ in /year/". 
+
+console.log(name + " is " + age + " in " + year);
+
+
+// 7. Declare a variable called isMarried and assign a boolean value. 
+
+const isMarried = false;
+
+// 8. With your declared variables, print the following "/name/ is married: true/false". 
+
+console.log(name + " is married: " + isMarried);
+
+
+// 9. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.  Print "JavaScript is fun: true" to the console. 
+
+let programmingLanguage = "Javascript";
+let isFun = true;
+console.log(programmingLanguage + " is fun:" + isFun);
+
+
+// 10. Print the J and S of JavaScript from the variable programmingLanguage to the console. 
+
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+
+// 11. Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage.length);
